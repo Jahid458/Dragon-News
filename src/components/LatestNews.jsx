0a1217414ee64da9e1831fd5d66728164b3f6de5
Jahid458 <jahidul898@outlook.com>
2024-11-14 20:31:06 +0600

    home marquee layout

--- src/components/LatestNews.jsx
+++ src/components/LatestNews.jsx
@@ -1,8 +1,19 @@
+import Marquee from "react-fast-marquee";
+import { Link } from "react-router-dom";
 
 const LatestNews = () => {
   return (
-    <div>
-      
+    <div className="flex gap-2 items-center bg-base-200 p-2">
+        <p className="bg-[#D72050] test-base-100 px-3 py-1">Latest</p>
+        <Marquee pauseOnHover={true} speed={100} className="space-x-10">
+         <Link to="/news">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae, facilis?
+         </Link>
+         <Link to="/news">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae, facilis?
+         </Link>
+         <Link to="/news">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae, facilis?
+         </Link>
+
+        </Marquee>
     </div>
   );
 };
