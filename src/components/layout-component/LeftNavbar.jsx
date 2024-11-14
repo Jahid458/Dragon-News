abc02bab0ec99f2f84904860daeeb0b7ae4c9ecb
Jahid458 <jahidul898@outlook.com>
2024-11-15 02:55:47 +0600

    category load section by id

--- src/components/layout-component/LeftNavbar.jsx
+++ src/components/layout-component/LeftNavbar.jsx
@@ -17,11 +17,15 @@ const LeftNavbar = () => {
   return <div>
     <h2 className="font-semibold mb-2">All Caterogy ({categories.length})</h2>
     <div className="flex flex-col gap-2">
-      {
-        categories.map(category => <NavLink to={`/category/${category.category_id}`} className="btn" key={category.category_id}>
-          {category.category_name}
-        </NavLink>)
-      }
+    {categories.map((category) => (
+          <NavLink
+            to={`/category/${category.category_id}`}
+            className="btn bg-base-100 border-none"
+            key={category.category_id}
+          >
+            {category.category_name}
+          </NavLink>
+        ))}
     </div>
   </div>;
 };
--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,3 +1,5 @@
+
+import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
 import LatestNews from "../components/LatestNews";
 import LeftNavbar from "../components/layout-component/LeftNavbar";
@@ -16,12 +18,14 @@ const HomeLayout = () => {
       <nav className="w-11/12 mx-auto py-2">
           <Navbar></Navbar>
       </nav>
-      {/* you......... */}
       <main className="w-11/12 mx-auto pt-5 grid md:grid-cols-12 gap-3">
         <aside className="left col-span-3">
           <LeftNavbar></LeftNavbar>
         </aside>
-        <section className="col-span-6">Main Content</section>
+        <section className="col-span-6">
+          
+          <Outlet></Outlet>
+        </section>
         <aside className="col-span-3">
           <RightNavbar></RightNavbar>
         </aside>
