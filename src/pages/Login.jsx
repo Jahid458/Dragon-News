5f1b8f0f939e179d4b9e9744eee365a404a5e5a6
Jahid458 <jahidul898@outlook.com>
2024-11-15 19:19:56 +0600

    set error in name in reg form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,31 +1,30 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { AuthContext } from "../provider/AuthProvider";
 
 const Login = () => {
-
-  const {userLogIn,setUser} = useContext(AuthContext);
+  const { userLogIn, setUser } = useContext(AuthContext);
+  const [error, setError] = useState({});
   const location = useLocation();
   const navigate = useNavigate();
-  console.log(location)
-  const handleSubmmit =(e)=>{
+  console.log(location);
+  const handleSubmmit = (e) => {
     e.preventDefault();
     const form = e.target;
     const email = form.email.value;
     const password = form.password.value;
 
-    console.log({email,password});
-    userLogIn(email,password)
-    .then(result =>{
-      const user = result.user;
-      setUser(user)
-      navigate(location?.state ? location.state : "/")
-    })
-    .catch((error)=>{
-     alert(error.code)
-
-    })
-  } 
+    console.log({ email, password });
+    userLogIn(email, password)
+      .then((result) => {
+        const user = result.user;
+        setUser(user);
+        navigate(location?.state ? location.state : "/");
+      })
+      .catch((err) => {
+        setError({ ...error, login: err.code });
+      });
+  };
 
   return (
     <div className="min-h-screen flex justify-center items-center">
@@ -33,7 +32,7 @@ const Login = () => {
         <h2 className="text-2xl font-semibold text-center">
           Login Your Account
         </h2>
-        <form onSubmit={handleSubmmit}  className="card-body">
+        <form onSubmit={handleSubmmit} className="card-body">
           <div className="form-control">
             <label className="label">
               <span className="label-text">Email</span>
@@ -57,11 +56,7 @@ const Login = () => {
               className="input input-bordered"
               required
             />
-            <label className="label">
-              <a href="#" className="label-text-alt link link-hover">
-                Forgot password?
-              </a>
-            </label>
+            {error.login && <label className="label text-sm text-red-600">{error.login}</label>}
           </div>
           <div className="form-control mt-6">
             <button className="btn btn-neutral rounded-none">Login</button>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,17 +1,22 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../provider/AuthProvider";
 
 const Register = () => {
 
   const {createNewUser,  setUser} = useContext(AuthContext)
-
+  const [error,setError] = useState({})
+  
 
   const handleSubmit =(e)=>{
     e.preventDefault();
     //get from data 
     const form = new FormData(e.target);
     const name = form.get("name")
+    if(name.length <5){
+      setError({...error, name:"must be more than 5 character long!"})
+      return
+    }
     const email = form.get("email")
     const photo = form.get("photo")
     const password = form.get("password")
@@ -52,6 +57,14 @@ const Register = () => {
               required
             />
           </div>
+          {
+            error.name && (
+              <label className="label text-xs text-red-600">
+                {error.name}
+              </label>
+            )
+          }
+
           <div className="form-control">
             <label className="label">
               <span className="label-text">Photo URL</span>
