035d8181a1bd5b399a5cd6d822d8076acfc91f0a
Jahid458 <jahidul898@outlook.com>
2024-11-15 17:21:20 +0600

    observer function

---
+++ src/firebase/firenase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA7CNY3HJn1u9V4Wj3STAzawk9KH92U0j4",
+  authDomain: "dragon-news-2269b.firebaseapp.com",
+  projectId: "dragon-news-2269b",
+  storageBucket: "dragon-news-2269b.firebasestorage.app",
+  messagingSenderId: "483687454730",
+  appId: "1:483687454730:web:d8edd132c1ae2c3aaca7f3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
