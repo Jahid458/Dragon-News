52d1796ab66a96fbe560adeb4f33bffad2449508
Jahid458 <jahidul898@outlook.com>
2024-11-15 18:15:44 +0600

    news detaiils

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,49 +1,53 @@
-import { createBrowserRouter,Navigate } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 import HomeLayout from "../layouts/HomeLayout";
-import CategoryNews from './../pages/CategoryNews';
+import CategoryNews from "./../pages/CategoryNews";
 import AuthLayout from "../layouts/AuthLayout";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
+import NewsDetails from './../pages/NewsDetails';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HomeLayout></HomeLayout>,
-    children:[
+    children: [
       {
-        path:"",
-        element:<Navigate to="/category/01"></Navigate>
+        path: "",
+        element: <Navigate to="/category/01"></Navigate>,
       },
       {
-        path:"/category/:id",
+        path: "/category/:id",
         element: <CategoryNews></CategoryNews>,
-        loader:({params})=>fetch(`https://openapi.programming-hero.com/api/news/category/${params.id}`)
-      }
-    ]
-     
+        loader: ({ params }) =>
+          fetch(
+            `https://openapi.programming-hero.com/api/news/category/${params.id}`
+          ),
+      },
+    ],
   },
   {
-    path: "/news",
-    element: <h1>News Layout</h1>,
+    path: "/news/:id",
+    element: <NewsDetails></NewsDetails>,
+    loader:({params})=>fetch(`https://openapi.programming-hero.com/api/news/${params.id}`)
   },
   {
     path: "auth",
     element: <AuthLayout></AuthLayout>,
-    children:[
+    children: [
       {
-        path:'/auth/login',
-        element: <Login></Login>
+        path: "/auth/login",
+        element: <Login></Login>,
       },
       {
-        path:'/auth/register',
-        element: <Register></Register>
-      }
-    ]
+        path: "/auth/register",
+        element: <Register></Register>,
+      },
+    ],
   },
   {
     path: "*",
     element: <h1>Error</h1>,
-  }
+  },
 ]);
 
 export default router;
